2bbe07a5d22f800054462f99fed4db4c7df681fb
Zioxs- <119772194+Zioxs@users.noreply.github.com>
2024-11-03 20:30:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const noteTitle = document.getElementById('note-title'); 
+const noteContent = document.getElementById('note-content');
 
+document.querySelector('form button').addEventListener('click', addNote); 
+
+function addNote() {
+  // Get title and content
+  const title = noteTitle.value;
+  const content = noteContent.value;
+  
+  // Create new note object
+  const newNote = {
+    title,
+    content
+  };
+  
+  // Add note to array of notes
+  notes.push(newNote);
+  
+  // Render new note
+  renderNote(newNote);
+}
